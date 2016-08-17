321e34d0dd81164cfa2c6696158700a45f871dba
David <dbstern@gwmail.gwu.edu>
2016-08-17 19:19:32 +0000

    formatting fix

--- estimate_abundance.py
+++ estimate_abundance.py
@@ -20,16 +20,16 @@ if __name__ == "__main__":
     if len(sys.argv) == 4:
         DIR, PATH, reads = sys.argv[1:]
         for dir in os.listdir(DIR):
-            if dir.startswith('COG') and os.path.isdir(dir):
+            if dir.endswith('.trinity') and os.path.isdir(dir):
                 fasta = str(DIR)+'/'+dir+'/Trinity.fasta'
-                outprefix = dir.split('_')[0]
+                outprefix = dir.split('.')[1]
                 rsemSE(PATH, transcripts=fasta, reads=reads, outprefix=outprefix)
     elif len(sys.argv) == 5:
         DIR, PATH, left_reads, right_reads = sys.argv[1:]
         for dir in os.listdir(DIR):
-            if dir.startswith('COG') and os.path.isdir(dir):
+            if dir.endswith('.trinity') and os.path.isdir(dir):
                 fasta = str(DIR)+'/'+dir+'/Trinity.fasta'
-                outprefix = dir.split('_')[0]
+                outprefix = dir.split('.')[1]
                 rsemPE(PATH, transcripts=fasta, left_reads=left_reads, right_reads=right_reads, outprefix=outprefix)
     else:
         print "usage: python estimate_abundance.py <DIR with Trinity output directories> <PATH to Trinity 'align_and_estimate_abundance.pl'> <fastq file/s>"
