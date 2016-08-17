5b07196eac890e6d57659a03c0c8165b3a815d95
David <dbstern@gwmail.gwu.edu>
2016-08-17 20:09:42 +0000

    fixed formatting

--- estimate_abundance.py
+++ estimate_abundance.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 '''
-This will use bowtie2 and RSEM to estimate abundance for each individual COG00000_Trinity.fasta given 
-a directory of Trinity output directories starting with COG
+This will use bowtie2 and RSEM to estimate abundance for each individual SRR0000.COG00000.assembled.fasta given 
+a directory of Trinity output files
 '''
 import sys, os
  
@@ -22,15 +22,15 @@ if __name__ == "__main__":
         for assembly in os.listdir(DIR):
             if assembly.endswith('.assembled.fasta'):
  #               fasta = str(DIR)+'/'+dir+'/Trinity.fasta'
-                outprefix = assembly.split('.')[1]
+                outprefix = assembly.split('.')[0]+'.'+assembly.split('.')[1]
                 rsemSE(PATH, transcripts=assembly, reads=reads, outprefix=outprefix)
     elif len(sys.argv) == 5:
         DIR, PATH, left_reads, right_reads = sys.argv[1:]
         for assembly in os.listdir(DIR):
             if assembly.endswith('.assembled.fasta'):
 #                fasta = str(DIR)+'/'+dir+'/Trinity.fasta'
-                outprefix = assembly.split('.')[1]
+                outprefix = assembly.split('.')[0]+'.'+assembly.split('.')[1]
                 rsemPE(PATH, transcripts=assembly, left_reads=left_reads, right_reads=right_reads, outprefix=outprefix)
     else:
         print "usage: python estimate_abundance.py <DIR with Trinity output directories> <PATH to Trinity 'align_and_estimate_abundance.pl'> <fastq file/s>"
-        sys.exit()
+        sy
