7e7155d111ee1a4b48a6d8ea1926db6b3dd64a16
Matthew Bendall <bendall@gwu.edu>
2016-08-17 21:59:31 +0000

    parallel in background

--- bin/blastCOGS.sh
+++ bin/blastCOGS.sh
@@ -9,5 +9,5 @@ export PATH=/home/ubuntu/sratoolkit.2.7.0-centos_linux64/bin:$PATH
 prefetch $sraid
 
 # Launch BLAST in parallel
-ls $cogpath/COG????.fa | parallel --progress -j $(nproc) "tblastn_vdb -db $sraid -query {} -max_target_seqs 1000000 -outfmt 6 -out $sraid.{/.}.out"
+ls $cogpath/COG????.fa | parallel --bg -j $(nproc) "tblastn_vdb -db $sraid -query {} -max_target_seqs 1000000 -outfmt 6 -out $sraid.{/.}.out"
 
