6252c1717b8217003e478d038247f2e67cf95275
Matthew Bendall <bendall@gwu.edu>
2016-08-25 18:29:35 -0400

    minimal setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='metatate',
+      version='0.1',
+      description='MetaTate: (Meta)transcriptome anno(tate)ion?',
+      url='https://github.com/NCBI-Hackathons/Metagenomic_Transcriptomes',
+      author='metagenomics team',
+      license='MIT',
+      packages=['metatate'],
+      zip_safe=False)
