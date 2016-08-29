af34a8fdc668f34d3f4f2f602f00df57bd1273e4
Matthew Bendall <bendall@gwu.edu>
2016-08-29 11:18:37 -0400

    handling eutils quirks

--- python/build_kegg.py
+++ python/build_kegg.py
@@ -1,7 +1,35 @@
 #! /usr/bin/env python
 
+import sys, re, time
 import urllib2
-import re
+
+FETCHSIZE = 100
+
+def attempt_url_open(url, max_attempts=5, quiet=False):
+    ''' Attempt to open URL multiple times.
+        This is useful in cases when urllib2.urlopen may fail randomly. For example, NCBI
+        eutils sometimes fail when submitting an efetch query if it occurs too soon after
+        the esearch query.
+            max_attempts    - maximum number of attempts
+            quiet           - if False, print information about failed attempts
+        Returns file-like object if successful and None if URL could not be opened
+    '''
+    attempt = 1
+    while attempt <= max_attempts:
+        try:
+            response = urllib2.urlopen(url)
+            return response
+        except urllib2.HTTPError as err:
+            if not quiet:
+                print >>sys.stderr, '        >>> Failed attempt %d' % attempt
+                print >>sys.stderr, '        >>> HTTP Error: %s' % err.code
+                print >>sys.stderr, '        >>> URL: %s' % url
+            time.sleep(5)
+    print >>sys.stderr, '        >>> Max attempts exceeded'
+    print >>sys.stderr, '        >>> Failed to open URL: %s' % url    
+    return None
+
+
 
 """ Functions for interacting with NCBI eutils """
 def eutil_url(app, db, args):
@@ -37,36 +65,62 @@ def fmt_seq(seq, lwidth=70):
     '''
     return '\n'.join(seq[i:i+lwidth] for i in range(0,len(seq), lwidth))
 
-def protein_accn_to_protein_seq(accn_list, chunksize=100):
+def protein_accn_to_protein_seq(accn_list):
     ''' Retrieve protein sequences for accession or accession list
             accn_list   - list of accession numbers
             chunksize   - number of accessions to process for each request
-        Generator yields tuples: (sequence_name, sequence)
+        Returns generator that yields (sequence_name, sequence)
     '''
     if type(accn_list) is str:
         accn_list = [ accn_list ]
-    for i in range(0, len(accn_list), chunksize):
-        search_args = {'term': '+OR+'.join('%s[accn]' % accn for accn in accn_list[i:i+chunksize]),
-                       'usehistory': 'y',
-                      }
-        search_url = eutil_url('esearch', 'protein', search_args)
-        data = urllib2.urlopen(search_url).read()
-        # Parse out the history tokens
-        fetch_args = {'WebEnv':   re.search('<WebEnv>(\S+)</WebEnv>', data).group(1),
-                      'query_key': re.search('<QueryKey>(\d+)</QueryKey>', data).group(1),
-                      'rettype': 'fasta',
-                      'retmode': 'text',
-                     }
-        fetch_url = eutil_url('efetch', 'protein', fetch_args)
-        for sname,seq in read_fasta(urllib2.urlopen(fetch_url)):
-            yield sname.strip('>'), seq
+    if len(accn_list) > FETCHSIZE:
+        print >>sys.stderr, "WARNING: Requesting %d accessions (Recommended size is <=%d)." % (len(accn_list), FETCHSIZE)
+            
+    search_args = {'term': '+OR+'.join('%s[accn]' % accn for accn in accn_list),
+                   'usehistory': 'y',
+                  }
+    search_url = eutil_url('esearch', 'protein', search_args)
+    
+    fetch_args = None
+    ncbi_error = 5
+    while ncbi_error > 0:    
+        response = attempt_url_open(search_url)
+        if response is None:
+            return
+        data = response.read()
+        m = re.search('<ERROR>(.+)</ERROR>', data)
+        if m:
+            ncbi_error = ncbi_error - 1
+            continue
+        else:
+            try:
+                fetch_args = {'rettype':   'fasta',
+                              'retmode':   'text',
+                              'WebEnv':    re.search('<WebEnv>(\S+)</WebEnv>', data).group(1),
+                              'query_key': re.search('<QueryKey>(\d+)</QueryKey>', data).group(1),
+                              }
+                ncbi_error = 0
+            except AttributeError:
+                ncbi_error = ncbi_error - 1
+    
+    # Fetch sequences
+    assert fetch_args is not None
+    fetch_url = eutil_url('efetch', 'protein', fetch_args)
+    response = attempt_url_open(fetch_url)
+    if response is None:
+        return
+    else:
+        return read_fasta(response)
 
 """ Functions for interacting with KEGG API """
 def all_KOs():
-    ''' Retrieve all KEGG Orthology functions from KEGG'''
-    for l in urllib2.urlopen('http://rest.kegg.jp/list/ko'):
-        if l:
-            yield l.strip('\n').split('\t')
+    ''' Retrieve all KEGG Orthology functions from KEGG
+        Returns a generator expression that yields (ko_id, description)
+    '''
+    response = attempt_url_open('http://rest.kegg.jp/list/ko')
+    if response is None:
+        return
+    return (tuple(l.strip('\n').split('\t')) for l in response if l.strip())
 
 def kgenes_for_KO(ko_id):
     ''' Retrieve all KEGG genes for ortholog group
@@ -74,8 +128,10 @@ def kgenes_for_KO(ko_id):
         Returns generator that yields KEGG gene IDs
     '''
     ko_id = ko_id if ko_id.startswith('ko:') else 'ko:%s' % ko_id
-    url = 'http://rest.kegg.jp/link/genes/%s' % ko_id
-    for l in urllib2.urlopen(url):
+    response = attempt_url_open('http://rest.kegg.jp/link/genes/%s' % ko_id)
+    if response is None:
+        return
+    for l in response:
         m = re.match('%s\t(\S+)' % ko_id, l)
         if m: yield m.group(1)
 
@@ -88,7 +144,10 @@ def ncbi_protein_accn_for_kgenes(kgene_ids, chunksize=10):
         kgene_ids = [ kgene_ids ]
     for i in range(0,len(kgene_ids),chunksize):
         url = 'http://rest.kegg.jp/conv/ncbi-proteinid/%s' % '+'.join(kgene_ids[i:i+chunksize])
-        for l in urllib2.urlopen(url):
+        response = attempt_url_open(url)
+        if response is None:
+            continue
+        for l in response:
             m = re.match('(\S+)\tncbi-proteinid:(\S+)', l)
             if m: yield m.groups()
 
@@ -110,7 +169,9 @@ def main(args):
         print >>sys.stderr, 'Downloading KO ids'    
         ko_ids = []
         with open(ko_txt, 'w') as outh:
-            for ko_id, description in all_KOs():
+            ko_desc = list(all_KOs())
+            if not ko_desc: sys.exit('>>> Failed to download KO ids')
+            for ko_id, description in ko_desc:
                 ko_ids.append(ko_id)
                 print >>outh, '%s\t%s' % (ko_id, description)
         print >>sys.stderr, 'Downloaded %d KO ids, saved to %s' % (len(ko_ids), ko_txt)
@@ -129,9 +190,12 @@ def main(args):
             accn_list = [l.strip('\n').split('\t')[1] for l in open(gene_txt, 'rU')]
             print >>sys.stderr, '    Found %d accessions in %s' % (len(accn_list), gene_txt)
         else:
-            print >>sys.stderr, '    Downloading gene list for %s' % ko_id        
+            print >>sys.stderr, '    Downloading gene list for %s' % ko_id
+            kgene_ids = list(kgenes_for_KO(ko_id))
+            if not kgene_ids:
+                print >>sys.stderr, '    >>> Failed to download gene list for %s' % ko_id
+                continue
             with open(gene_txt, 'w') as outh:
-                kgene_ids = list(kgenes_for_KO(ko_id))
                 accn_list = []
                 for kgid,accn in ncbi_protein_accn_for_kgenes(kgene_ids):
                     print >>outh, '%s\t%s' % (kgid,accn)
@@ -142,9 +206,16 @@ def main(args):
         gene_faa = '%s/%s.faa' % (args.dest, ko_id.split(':')[1])
         seqcount = 0
         with open(gene_faa, 'w') as outh:
-            for n,s in protein_accn_to_protein_seq(accn_list):
-                seqcount += 1
-                print >>outh, '>%s\n%s' % (n, fmt_seq(s))
+            for i in range(0, len(accn_list), FETCHSIZE):
+                accn_subset = accn_list[i:i+FETCHSIZE]
+                iter = protein_accn_to_protein_seq(accn_subset)
+                if iter is None:
+                    print >>sys.stderr, '    >>> Sequence download failed for %d sequences.\n%s' % (len(accn_subset), ','.join(accn_subset))
+                    continue
+                for n,s in iter:
+                    seqcount += 1
+                    print >>outh, '>%s\n%s' % (n, fmt_seq(s))
+        
         print >>sys.stderr, '    Wrote %d sequences to %s' % (seqcount, gene_faa)
 
 if __name__ == '__main__':
